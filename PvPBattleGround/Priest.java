afbcf6ff6db70daf35063cb34cf41b2e870ad753
popliarman2019 <42761596+popliarman2019@users.noreply.github.com>
2019-03-05 08:11:56 -0800

    Create Priest.java

---
+++ PvPBattleGround/Priest.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class Priest extends PlayerCharactar {
+
+
+
+    public Priest(String newName) {
+        System.out.println("\nA priest named " + newName + " has been created.");
+
+        name = newName;
+        Random rand = new Random();
+        strength = 15 + (rand.nextInt(6)+1);
+        intelligence = 30 + 2*(rand.nextInt(6)+1);
+        agility = 15 + (rand.nextInt(6)+1);
+
+        dispStats();
+
+    }
+
+    public void dispStats() {
+        System.out.println(name + " is a priest of strength: " + strength);
+        System.out.println(name + " is a priest of intelligence: " + intelligence);
+        System.out.println(name + " is a priest of agility: " + agility);
+    }
+
+
+}
