247a9be2f35e9a127f3dea600d1bfc8c57595463
popliarman2019 <42761596+popliarman2019@users.noreply.github.com>
2019-03-05 08:10:34 -0800

    Create PlayerCharactar.java

---
+++ PvPBattleGround/PlayerCharactar.java
@@ -0,0 +1,15 @@
+public abstract class PlayerCharactar {
+
+    String name;
+
+    int strength, agility, intelligence;
+
+    int hp, hpmax;
+    int mp, mpmax;
+
+
+    public PlayerCharactar() {
+        System.out.println("Player Charactar extended class created.");
+    }
+
+}
