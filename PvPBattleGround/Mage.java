69922b4e8842c9756c9121a0eaf32777f4cb108a
popliarman2019 <42761596+popliarman2019@users.noreply.github.com>
2019-03-05 08:11:34 -0800

    Create Mage.java

---
+++ PvPBattleGround/Mage.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Mage extends PlayerCharactar {
+
+
+
+    public Mage(String newName) {
+        System.out.println("\nA mage named " + newName + " has been created.");
+
+        name = newName;
+        Random rand = new Random();
+        strength = 15 + (rand.nextInt(6)+1)/2;
+        intelligence = 15 + 2*(rand.nextInt(6)+1);
+        agility = 15 + (rand.nextInt(6)+1);
+
+        dispStats();
+
+    }
+
+    public void dispStats() {
+        System.out.println(name + " is a mage of strength: " + strength);
+        System.out.println(name + " is a mage of intelligence: " + intelligence);
+        System.out.println(name + " is a mage of agility: " + agility);
+    }
+}
