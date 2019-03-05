d6417152b33f286348f319d6cf42247db532a54d
popliarman2019 <42761596+popliarman2019@users.noreply.github.com>
2019-03-05 08:10:00 -0800

    Create Main.java

---
+++ PvPBattleGround/Main.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int val = 5;
+        Random rand = new Random();
+
+        Fighter fighter1 = new Fighter("Rethgif");
+        Fighter fighter2 = new Fighter ("Catty");
+
+        Priest priest1 = new Priest ("Rhonda");
+        Priest priest2 = new Priest ("Wanda");
+
+
+        Paladin paladin1 = new Paladin ("Nidalap");
+        Paladin paladin2 = new Paladin ("Sammy");
+
+        Mage mage1 = new Mage ("Egam");
+        Mage mage2 = new Mage ("Sally");
+
+//        PlayerCharactar playerx = new PlayerCharactar("Jonny");
+
+    }
+
+}
